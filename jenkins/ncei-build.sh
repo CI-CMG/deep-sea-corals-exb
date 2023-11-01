3c4827c1e0d008276d9381d34462bb50c0358e8c
jccartwright <john.c.cartwright@comcast.net>
2023-11-01 16:02:49 -0600

    modified unzip, fix permissions, and re-zip prior to deployment

--- jenkins/ncei-build.sh
+++ jenkins/ncei-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # "npm audit fix" returns a non-0 exit code if any vulnerability cannot be addressed automatically
 # "set -e" caused build to fail in this case 
 #set -ex
@@ -18,16 +19,16 @@ cp -r widgets/* arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder
 # Create App directory
 mkdir -p arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/server/public/apps
 cp -r apps/* arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/server/public/apps
-  # NPM install client folder
+# NPM install client folder
 cd arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/client
 npm ci
 npm audit fix
-cd $WORKSPACE
+cd ${WORKSPACE}
 # NPM install in server folder
 cd arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/server
 npm ci
 npm audit fix
-cd $WORKSPACE
+cd ${WORKSPACE}
 # install any widget dependencies
 cd arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/client/your-extensions/widgets
 for d in `find . -mindepth 1 -maxdepth 1 -type d`; do
@@ -39,25 +40,42 @@ for d in `find . -mindepth 1 -maxdepth 1 -type d`; do
   fi
   cd ..
 done
-cd $WORKSPACE
+
+cd ${WORKSPACE}
 
 # Build widgets
 cd arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/client
 npm run build:dev
-cd $WORKSPACE
+cd ${WORKSPACE}
 
 # Run download script
 cd arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder
 node -e "require('./server/src/middlewares/dev/apps/app-download.js').zipApp('0', 'app.zip')"
 
-cd $WORKSPACE
+# Unzip the app.zip and remove the original
+unzip -q app.zip -d app_unzipped
+rm app.zip
+
+# Fix file permissions
+cd app_unzipped/
+find . -type d -exec chmod 0775 {} \;
+find . -type f -exec chmod 0664 {} \;
+
+# Re-zip the app
+zip -qr ../app.zip *
+
+# Clean up the temporary unzipped directory
+cd ..
+rm -rf app_unzipped
+
+cd ${WORKSPACE}
 mkdir dist
 mv -i arcgis-experience-builder-${EXB_VERSION}/ArcGISExperienceBuilder/app.zip dist
-echo "unzip app.zip and grant o+rx to directories and o+r to files" > dist/README
+
+# echo "unzip app.zip and grant o+rx to directories and o+r to files" > dist/README
 if [ -f dist/app.zip ]; then
-  # TODO deploy app.zip?
   echo "successfully built artifact app.zip"
 else
   echo "ERROR: artifact app.zip not found"
   exit 1
-fi 
+fi
